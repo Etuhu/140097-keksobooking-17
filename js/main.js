7863d47a18c4c87d86c5e3341c6894916eae1be9
Etuhu <andgiz@mail.ru>
2019-06-21 23:26:14 +0700

    Выполнено задание по теме 5. Личный проект: пока все дома

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var TYPES_OF_HOUSING = ['palace', 'flat', 'house', 'bungalo'];
+var COORDINATE_Y_MIN = 130;
+var COORDINATE_Y_MAX = 630;
+
 var mapWidth = document.querySelector('.map').offsetWidth;
+var mapPinWidth = document.querySelector('.map__pin').offsetWidth;
+
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
@@ -12,16 +17,30 @@ var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+var getUniqueNumber = function (max) {
+  var sum = 0;
+  for (var i = 1; i <= max; i++) {
+    sum += 1;
+  }
+  return sum;
+};
+
 var createOfferings = function (count) {
   var offerings = [];
 
   for (var i = 0; i < count; i++) {
     offerings.push({
-      author: {avatar: '../img/avatars/user0' + getRandom(1, 8) + '.png'},
-      offer: {type: TYPES_OF_HOUSING[getRandom(0, TYPES_OF_HOUSING.length - 1)]},
+      author: {
+        avatar: 'img/avatars/user0' + getUniqueNumber(i + 1) + '.png'
+      },
+      offer: {
+        type: TYPES_OF_HOUSING[getRandom(0, TYPES_OF_HOUSING.length - 1)]
+      },
       location:
-        {x: getRandom(0, mapWidth),
-          y: getRandom(130, 630)}
+        {
+          x: getRandom(mapPinWidth, mapWidth - mapPinWidth),
+          y: getRandom(COORDINATE_Y_MIN, COORDINATE_Y_MAX)
+        }
     });
   }
   return offerings;
@@ -30,8 +49,8 @@ var createOfferings = function (count) {
 var renderMapPin = function (offering) {
   var mapPinElement = pinTemplate.cloneNode(true);
 
-  mapPinElement.querySelector('.map__pin').style.left = offering.location.x + 'px';
-  mapPinElement.querySelector('.map__pin').style.top = offering.location.y + 'px';
+  mapPinElement.style.left = offering.location.x + 'px';
+  mapPinElement.style.top = offering.location.y + 'px';
   mapPinElement.querySelector('img').src = offering.author.avatar;
   mapPinElement.querySelector('img').alt = offering.offer.type;
 
@@ -39,9 +58,9 @@ var renderMapPin = function (offering) {
 };
 
 var drawingMapPin = function () {
-  var offers = createOfferings(8);
-  for (var i = 0; i < offers.length; i++) {
-    fragment.appendChild(renderMapPin(createOfferings[i]));
+  var offersArray = createOfferings(8);
+  for (var i = 0; i < offersArray.length; i++) {
+    fragment.appendChild(renderMapPin(offersArray[i]));
   }
   mapPins.appendChild(fragment);
 };
